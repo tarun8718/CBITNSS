25c1c88b200500cbb04f976c7a0302790017b8f1
tarun8718 <tarunkumar8718@gmail.com>
2021-05-01 23:22:44 +0530

    then()

--- index.js
+++ index.js
@@ -10,40 +10,37 @@ const MONGOURI = require("./keys");
 mongoose.connect(MONGOURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
-mongoose.connection.on("connected", () => {
-  console.log("conneted to mongo successfully");
-});
-mongoose.connection.on("error", (err) => {
-  console.log("err connecting", err);
-});
-
-const app = express();
-server = http.createServer(app);
-
-var db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function () {});
-
-app.use(
-  session({
-    secret: "secret hai bro nahi bol saktha",
-    resave: true,
-    saveUninitialized: false,
-    store: MongoStore.create({
-      mongoUrl: MONGOURI,
-    }),
-  })
+}).then(
+    () => {
+          const app = express();
+          server = http.createServer(app);
+          
+          var db = mongoose.connection;
+          db.on("error", console.error.bind(console, "connection error:"));
+          db.once("open", function () {});
+          
+          app.use(
+            session({
+              secret: "secret hai bro nahi bol saktha",
+              resave: true,
+              saveUninitialized: false,
+              store: MongoStore.create({
+                mongoUrl: MONGOURI,
+              }),
+            })
+          );
+          
+          app.use(express.static(path.join(__dirname, "views")));
+          
+          app.use(bodyParser.json());
+          app.use(bodyParser.urlencoded({ extended: false }));
+          
+          const main = require("./Routes/main");
+          app.use("/", main);
+          
+          const PORT = process.env.PORT || 3000;
+          
+          server.listen(PORT, () => console.log(`Server listening on ${PORT}`));
+          
+    }
 );
-
-app.use(express.static(path.join(__dirname, "views")));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-const main = require("./Routes/main");
-app.use("/", main);
-
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => console.log(`Server listening on ${PORT}`));
