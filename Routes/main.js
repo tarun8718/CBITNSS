28e2164285d8890c8f5d1086260c23bc208c1eb8
tarun8718 <tarunkumar8718@gmail.com>
2021-05-01 22:15:00 +0530

    fix keys

--- Routes/main.js
+++ Routes/main.js
@@ -8,9 +8,6 @@ router.get("/", function (req, res, next) {
 });
 
 router.post("/", function (req, res, next) {
-  console.log("Inside Signup Router");
-  console.log(req.body);
-
   const personInfo = req.body;
 
   User.findOne({ username: personInfo.username }, function (err, data) {
@@ -32,13 +29,10 @@ router.post("/", function (req, res, next) {
 });
 
 router.get("/table", function (req, res, next) {
-
-    var tabledata;
-    Donor.find({}).exec((err,data) => {
-        console.log("Data = ");
-        console.log(data);
-        return res.render("table.ejs", { 'data' : data});
-    });
+  var tabledata;
+  Donor.find({}).exec((err, data) => {
+    return res.render("table.ejs", { data: data });
+  });
 });
 
 router.get("/create", function (req, res, next) {
@@ -46,9 +40,6 @@ router.get("/create", function (req, res, next) {
 });
 
 router.post("/create", function (req, res, next) {
-    console.log("Inside create Router");
-  console.log(req.body);
-
   const donorInfo = req.body;
 
   Donor.findOne({ name: donorInfo.name }, function (err, data) {
@@ -57,7 +48,7 @@ router.post("/create", function (req, res, next) {
         name: donorInfo.name,
         phno: donorInfo.phno,
         bloodgroup: donorInfo.bloodgroup,
-        desc: donorInfo.desc
+        desc: donorInfo.desc,
       });
 
       newDonor.save(function (err, Person) {
--- index.js
+++ index.js
@@ -1,51 +1,49 @@
-const path = require('path');
-const express = require('express');
-const http = require('http');
-const bodyParser = require('body-parser');
-const mongoose  = require('mongoose');
-var session = require('express-session');
-var MongoStore = require('connect-mongo');
-const MONGOURI = require('./keys');
+const path = require("path");
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+var session = require("express-session");
+var MongoStore = require("connect-mongo");
+const MONGOURI = require("./keys");
 
-mongoose.connect(MONGOURI,{
-    useNewUrlParser:true,
-    useUnifiedTopology: true
-})
-mongoose.connection.on('connected',()=>{
-    console.log("conneted to mongo successfully");
-})
-mongoose.connection.on('error',(err)=>{
-    console.log("err connecting",err);
-})
-
-const app= express();
-server = http.createServer(app);
-
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
+mongoose.connect(MONGOURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on("connected", () => {
+  console.log("conneted to mongo successfully");
+});
+mongoose.connection.on("error", (err) => {
+  console.log("err connecting", err);
 });
 
-app.use(session({
-    secret: 'secret hai bro nahi bol saktha',
+const app = express();
+server = http.createServer(app);
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {});
+
+app.use(
+  session({
+    secret: "secret hai bro nahi bol saktha",
     resave: true,
     saveUninitialized: false,
     store: MongoStore.create({
-        mongoUrl: MONGOURI
-    })
-}));
+      mongoUrl: MONGOURI,
+    }),
+  })
+);
 
-
-app.use(express.static(path.join(__dirname,'views')));
+app.use(express.static(path.join(__dirname, "views")));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const main = require('./Routes/main');
-app.use('/', main);
-
+const main = require("./Routes/main");
+app.use("/", main);
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT,() => console.log(`Server listening on ${PORT}`));
+server.listen(PORT, () => console.log(`Server listening on ${PORT}`));
